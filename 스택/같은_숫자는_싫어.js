a9559792dc181b84dcc686f379a83902465139ca
Seongjun Lee <253eosam@gmail.com>
2022-09-01 16:32:37 +0900

    propose pr

--- 스택/같은_숫자는_싫어.js
+++ 스택/같은_숫자는_싫어.js
@@ -19,7 +19,7 @@ function myStack(size) {
     if (this.isFull()) throw Error("스택이 찼습니다.");
     this.status[++this.top] = val;
   };
-  this.myPop = () => {
+  this.myPop = () => { // pop은 후입선출을 하면서 빼는값을 리턴하는 역할도 합니다. 
     if (this.isEmpty()) throw Error("스택이 비었습니다.");
     this.status[this.top--] = null;
   };
@@ -41,6 +41,18 @@ function solution(arr) {
 
   for (let i=1; i<arr.length; i++) {
     (ms.myPeek() !== arr[i]) && ms.myPush(arr[i]);
+    /*
+    매니저님은 아래 두 문법중에서 어느것을 선호하시나요 ?
+```js
+  if (true) console.log("true")
+```
+
+```js
+  if (true) {
+      console.log("true")
+  }
+```
+    */
   }
   
   while(!ms.isEmpty()) {
